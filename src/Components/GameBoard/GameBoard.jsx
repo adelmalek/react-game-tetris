5da264012579edcb163ab9a66798774b9cd1b403
Adel <gerencser.adel@gmail.com>
2022-07-05 16:08:30 +0200

    render board with current shape

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import './GameBoard.css';
+import Square from '../Square/Square';
 
 export default class GameBoard extends Component {
+  renderBoard() {
+    const board = this.props.board;
+    const JSXboard = [];
+
+    for (let i = 0; i < board.length; i++) {
+      const row = board[i];
+      const JSXrow = [];
+      for (let j = 0; j < row.length; j++) {
+        const cell = row[j];
+        JSXrow.push(
+          <Square shapeColor={cell} hasBorder={true} key={cell +j} />
+        )
+      }
+      JSXboard.push(
+        <div className='row' key={row.toString() + i}>{JSXrow}</div>
+      )
+    };
+
+    return JSXboard;
+  }
   render() {
     return (
-      <div className='game-board'>{JSON.stringify(this.props.board)}</div>
+      <div className='game-board'>{this.renderBoard()}</div>
     )
   }
 }
