a04bd81dde011dddb7cb95302984413fc45c5384
Adel <gerencser.adel@gmail.com>
2022-07-05 19:38:11 +0200

    move tetromino right

--- src/App.js
+++ src/App.js
@@ -238,7 +238,13 @@ class App extends React.Component {
   }
 
   moveCurrentTetrominoRight() {
+    const proposedPosition = this.getCurrentCoordinates().map( ({row,col}) => ({row, col: col + 1}));
 
+    if (this.isTetrominoSpaceFree(proposedPosition)) {
+      this.setState({
+        colIndex: this.state.colIndex + 1
+      })
+    }
   }
 
   render() {
