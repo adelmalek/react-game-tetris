3264f9fb13c9d416144321609307c2516b25e4cb
Adel <gerencser.adel@gmail.com>
2022-07-05 15:46:24 +0200

    get current coordinates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import GameStats from './Components/GameStats/GameStats';
 import Options from './Components/Options/Options';
 import Score from './Components/Score/Score';
 
+import TETROMINO_SHAPES from './Constans/tetrominos';
+
 const BOARD_HEIGHT = 20;
 const BOARD_WIDTH = 10;
 
@@ -14,12 +16,23 @@ class App extends React.Component {
     return new Array(BOARD_HEIGHT).fill(null).map( (_) => new Array(BOARD_WIDTH).fill(null));
   }
 
+  getRandomTetrominoShape() {
+    const shape = ['I', 'O', 'J', 'L', 'S', 'Z', 'T'];
+    const index = Math.floor(Math.random() * 7);
+    return shape[index];
+  }
+
   state = {
     gameStarted: false,
     gameOver: false,
     paused: false,
     score: 0,
-    board: this.createEmptyBoard()
+    board: this.createEmptyBoard(),
+    currentRotationIndex: 0,
+    rowIndex: 0,
+    colIndex: 4,
+    currentShape: null,
+    nextShape: null
   }
 
   startGame = () => {
@@ -27,7 +40,13 @@ class App extends React.Component {
       gameStarted: true,
       gameOver: false,
       paused: false,
-      score: 0
+      score: 0,
+      currentRotationIndex: 0,
+      rowIndex: 0,
+      colIndex: 4,
+      board: this.createEmptyBoard(),
+      currentShape: this.getRandomTetrominoShape(),
+      nextShape: this.getRandomTetrominoShape()
     })
   }
 
@@ -35,6 +54,35 @@ class App extends React.Component {
     this.setState({
       paused: !this.state.paused
     })
+  }
+
+  getCurrentCoordinates(
+    rotationOffset=0,
+    currentShapeIndex=this.state.currentShape,
+    currentRotationIndex=this.state.currentRotationIndex,
+    rowIndex=this.state.rowIndex,
+    colIndex=this.state.colIndex
+  ) {
+    const currentShape = TETROMINO_SHAPES[currentShapeIndex];
+    if (typeof currentShape === 'undefined') {
+      return [];
+    }
+
+    const currentRotation = currentShape[(currentRotationIndex + rotationOffset) % currentShape.length];
+
+    const coordinates = [];
+    for (let i = 0; i < currentRotation.length; i++) {
+      for (let j = 0; j < currentRotation[i].length; j++) {
+        if (currentRotation[i][j]) {
+          coordinates.push({
+            row: rowIndex + i,
+            col: colIndex + j
+          })
+        }
+      }
+    };
+    
+    return coordinates;
   }
 
   render() {
