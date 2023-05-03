f5612609ae9a3fe52669383d6881d2de2dcf8932
unknown <amalek.devo@gmail.com>
2023-05-03 16:36:27 +0200

    score component

--- src/Components/Score/Score.jsx
+++ src/Components/Score/Score.jsx
@@ -4,7 +4,10 @@ import './Score.css';
 export default class Score extends Component {
   render() {
     return (
-      <div className='score'>Score: {this.props.score}</div>
+      <div className='score'>
+        Score:
+        <div>{this.props.score}</div>
+      </div>
     )
   }
 }
